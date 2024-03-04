d24dccc3ac4f7c9aa05ab6d473476d1688ac9dd4
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-03-04 12:13:59 -0800

    updated

--- src/Faq.js
+++ src/Faq.js
@@ -65,27 +65,25 @@ const Faq = () => {
       {faq.map((item) => (
         <div key={item.id}>
           <span>
-            {/* <button onClick={() => handleToggle(item.id)}> */}
-              <svg
-                onClick={() => handleToggle(item.id)}
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className={`w-6 h-6 ${item.isAnswerVisible ? "rotate" : ""}`}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d={
-                    item.isAnswerVisible
-                      ? "m4.5 15.75 7.5-7.5 7.5 7.5"
-                      : "m19.5 8.25-7.5 7.5-7.5-7.5"
-                  }
-                />
-              </svg>
-            {/* </button> */}
+            <svg
+              onClick={() => handleToggle(item.id)}
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className={`w-6 h-6 ${item.isAnswerVisible ? "rotate" : ""}`}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d={
+                  item.isAnswerVisible
+                    ? "m4.5 15.75 7.5-7.5 7.5 7.5"
+                    : "m19.5 8.25-7.5 7.5-7.5-7.5"
+                }
+              />
+            </svg>
           </span>
           <h3>{item.question}</h3>
           {item.isAnswerVisible && (
