6c1ec9ae5e5dd07ddf418d946ab2d5f93960bab0
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-08-02 11:44:17 +0100

    added a font and added some faq contents

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="font-abc text-gray-700 mt-28 md:flex flex-row-reverse  pb-32 ">
+    <div className="font-abc text-gray-700 mt-36 md:flex flex-row-reverse  pb-32 ">
       <div>
         <img
           src="/images/undraw_Programming_re_kg9v (1).png"
@@ -11,12 +11,16 @@ const Hero = () => {
         />
       </div>
       <div className="mt-20">
-        <h1 className="font-bold text-4xl ml-10 md:text-6xl md:ml-28">
-          More than just shorter links
+        <h1 className="font-bold text-5xl pb-3 ml-5 md:text-6xl md:ml-28">
+          More than just <br/>shorter links
         </h1>
-        <p className="ml-10 md:ml-28">
-          Build your brand's recognition and get detailed insights on how your
-          links are performing
+        <p className="ml-5 md:ml-28">
+          Build your brand's recognition and get detailed insights <br/>on how your
+          links are performing.Weblify serves as a user-friendly URL shortener
+          that simplifies the process of managing and <br/>shortening links,
+          including branded ones. <br/>Enhance your brand's growth and business
+          potential by utilizing short, memorable, and engaging links with
+          Weblify's versatile platform.
         </p>
         <button className="btn mt-10 ml-20 px-20 text-white hover:text-gray-600 md:ml-28 md:px-6 rounded-full bg-blue-300">
           Get Started
